dc2a5c9b17662640231a76be261b18f07a706f49
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2023-10-03 20:28:33 +0100

    Add NoConnectivityScree component

---
+++ bliss-challenge-frontend/src/components/NoConnectivityScreen/NoConnectivityScreen.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import NetworkStatus, { Offline, Online } from "react-network-status";
+
+function NoConnectivityScreen() {
+  return (
+    <div>
+      <h1>TESTE</h1>
+    </div>
+  );
+}
+export default NoConnectivityScreen;
