b6ffc9e4f44f6cc65ab65ee6a0346c85cca2ec60
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2023-10-04 13:37:01 +0100

    Add smtp

--- bliss-challenge-backend/app.js
+++ bliss-challenge-backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const bodyParser = require("body-parseer");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,10 +10,11 @@ app.use(bodyParser.json());
 app.post("/share", (req, res) => {
   const { to, subject, text } = req.body;
   const transporter = nodemailer.createTransport({
-    service: "",
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
     auth: {
-      user: "",
-      pass: "",
+      user: "6d7c97bd727cb4",
+      pass: "813e8cf8f3c0b6",
     },
   });
 
@@ -25,9 +26,9 @@ app.post("/share", (req, res) => {
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).json({ error: "Failed to send email" });
+    if (err) {
+      console.log(err);
+      res.status(500).json({ err: "Failed to send email" });
     } else {
       console.log(`Email sent: ${info.response}`);
       res.status(200).json({ message: "Email sent sucessfully" });
