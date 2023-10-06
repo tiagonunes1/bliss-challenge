74030f4f281c2ce1371f57e235367b28288519a4
tiagonunes1 <tiagoalexandrenunes1@gmail.com>
2023-10-06 19:54:01 +0100

    Remove the redudant ID from the QuestionCard component

--- bliss-challenge-frontend/src/components/QuestionCard/QuestionCard.js
+++ bliss-challenge-frontend/src/components/QuestionCard/QuestionCard.js
@@ -4,7 +4,6 @@ function QuestionCard({ question, onClickDetail, onShareQuestion }) {
   return (
     <div onClick={() => onClickDetail(question)} className="question-card">
       <h3>{question.question}</h3>
-      <p>ID: {question.id}</p>
       <p>Published: {new Date(question.published_at).toLocaleString()}</p>
     </div>
   );
